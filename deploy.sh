dd57985503024f42788099656093e18111f37b5b
Aton4ST <64774266+aimtyaem@users.noreply.github.com>
2025-03-05 01:21:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git init  # Initialize the repository (only needed once)
 git add .  # Stage all changes
 git commit -m "Updated Solar System 3D project"
-git branch -M main  # Ensure the branch is named 'main'
-git remote add origin https://github.com/YOUR_USERNAME/YOUR_REPO.git
-git push -u origin main
+git branch -M master  # Ensure the branch is named 'main'
+git remote add origin https://github.com/aimtyaem/RA-Space-exploration.git
+git push -u origin master
