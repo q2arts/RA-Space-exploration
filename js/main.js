f138580c47287eeda5380b1b0cfb0683f56416aa
Aton4ST <64774266+aimtyaem@users.noreply.github.com>
2025-03-08 01:22:34 +0200

    Delete js/main.js
    Related to another project of RA

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Import dependencies (if using ES modules)
-// import { initSolarSystem } from './solarSystem.js';
-// import { sendChat } from './chat.js';
-
-// Initialize the solar system
-initSolarSystem();
-
-// Attach event listeners
-document.querySelector('button').addEventListener('click', sendChat);
