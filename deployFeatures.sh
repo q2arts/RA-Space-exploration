73aaaadcc74737298ef20485558c9044bbbedc69
Aton4ST <64774266+aimtyaem@users.noreply.github.com>
2025-03-08 14:12:26 +0200

    Update deployFeatures.sh
    Updating index

--- deployFeatures.sh
+++ deployFeatures.sh
@@ -4,14 +4,14 @@ cd path/to/RA-Space-exploration
 # Create and edit index.html
 echo '<!DOCTYPE html>...' > index.html
 
-# Create and edit index.css
-echo 'body { ... }' > css/index.css
+# Create and edit home.css
+echo 'body { ... }' > css/home.css
 
-# Create and edit js/index.js
-echo 'let stressLevel = 30; ...' > js/index.js
+# Create and edit js/home.js
+echo 'let stressLevel = 30; ...' > js/home.js
 
 # Add the files to the repository
-git add index.html css/index.css js/index.js
+git add index.html css/home.css js/home.js
 
 # Commit the changes
 git commit -m "update project files"
